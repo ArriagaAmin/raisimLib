c32e5d68113bb3c34455a6ad25aed5e9ad4e0bd9
Eduardo López <32850455+eduardo98m@users.noreply.github.com>
2023-04-13 12:58:52 -0400

    Option to build with external raisim instalation

--- raisimGymTorch/build_windows.bat
+++ raisimGymTorch/build_windows.bat
@@ -1,3 +1,3 @@
 
 
-python .\setup.py develop  --CMAKE_PREFIX_PATH "C:\Users\edued\Documents\Universidad\Tesis\raisim_workspace\raisimLib\raisim\win32;C:/Users/edued/Documents/Universidad/Tesis/raisim_workspace/raisimLib/thirdParty/Eigen3/share/eigen3/cmake"
+python .\setup.py develop  --CMAKE_PREFIX_PATH "C:\Users\edued\Documents\Universidad\Tesis\raisim_workspace\raisimLib\raisim\win32;C:/Users/edued/Documents/Universidad/Tesis/raisim_workspace/raisimLib/thirdParty/Eigen3/share/eigen3/cmake" --USE_EXTERNAL_RAISIM
